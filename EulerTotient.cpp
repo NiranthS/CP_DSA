8c8ffefb1ba4c2034df503f070e18433b8e951de
niranth-s <56146514+niranth-s@users.noreply.github.com>
2020-10-23 18:42:31 +0530

    Update EulerTotient.cpp

--- EulerTotient.cpp
+++ EulerTotient.cpp
@@ -1,9 +1,7 @@
-int eulertotient(int n) {
-int result = n;
-for(int i=2;i*i <= n;i++) {
-if (n % i == 0) result -= result / i;
-while (n % i == 0) n /= i;
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Hello World";
 }
-if (n > 1) result -= result / n;
-return result;
-} 
